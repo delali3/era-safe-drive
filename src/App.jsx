95779e3c74e2d4dfe912394453f3549f2b405bce
Nusetor Foster <nusetorfoster77@gmail.com>
2025-06-13 05:12:01 +0000

    fix: update default route to render HomePage instead of LoginPage

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
